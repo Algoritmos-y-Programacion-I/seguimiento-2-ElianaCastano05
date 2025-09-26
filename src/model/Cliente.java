161b4206b07875852b84bcc99782b3f349d82a3b
ElianaCastano05 <eliana.castano@u.icesi.edu.co>
2025-09-26 12:44:55 -0500

    Versión 1

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,13 +1,34 @@
 package model;
 
 public class Cliente {
-    
 
+    //Atributos
 
-    public Cliente(){
+    private String nombre_cliente;
+    private int edad;
+    private String cedula;
+    private Cuenta[] cuentas_bancarias;
 
+    //Constructor
 
+    public Cliente(String nombre_cliente, int edad, String cedula){
+        this.nombre_cliente = nombre_cliente;
+        this.edad = edad;
+        this.cedula = cedula;
+        this.cuentas_bancarias = new Cuenta[10];
     }
+
+
+    // Metodos
+
+    public void agregarCuentaCliente(String nombre_banco, TipoDeCuenta tipo_cuenta, double saldo_actual){
+        for(int i = 0; i < cuentas_bancarias.length ; i++ ){
+            if(cuentas_bancarias[i] == null){
+                cuentas_bancarias[i] = new Cuenta(nombre_banco,tipo_cuenta,saldo_actual);
+            }
+        }
+    }
+
 
      /*
      * ATENCION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- src/model/Cuenta.java
+++ src/model/Cuenta.java
@@ -2,4 +2,58 @@ package model;
 
 public class Cuenta {
     
+    //Atributos
+
+    private String nombre_banco;
+    private TipoDeCuenta tipo_cuenta;
+    private double saldo_actual;
+
+    //Constructor
+
+    public Cuenta(String nombre_banco, TipoDeCuenta tipo_cuenta,double saldo_actual){
+
+        this.nombre_banco = nombre_banco;
+        this.tipo_cuenta = tipo_cuenta;
+        this.saldo_actual = saldo_actual;
+
+    }
+
+    //Metodos
+
+    public void depositarDineroCuenta(double deposito_saldo){
+        this.saldo_actual = saldo_actual + deposito_saldo;
+    }
+
+    public void retirarDineroCuenta(double retiro_saldo){
+       
+       if(saldo_actual >= 0 && saldo_actual > retiro_saldo){
+            this.saldo_actual = saldo_actual - retiro_saldo;
+       }
+    }
+
+    // Getter's y setter's
+
+    public String getNombre_banco() {
+        return nombre_banco;
+    }
+
+    public void setNombre_banco(String nombre_banco) {
+        this.nombre_banco = nombre_banco;
+    }
+
+    public TipoDeCuenta getTipo_cuenta() {
+        return tipo_cuenta;
+    }
+
+    public void setSaldo_actual(TipoDeCuenta tipo_cuenta){
+        this.tipo_cuenta = tipo_cuenta;
+    }
+
+    public double getSaldo_actual() {
+        return saldo_actual;
+    }
+
+    public void setSaldo_actual(double saldo_actual) {
+        this.saldo_actual = saldo_actual;
+    }
 }
